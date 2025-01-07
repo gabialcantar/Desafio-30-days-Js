b0c68c5a2659e625fd7a5f493866ee2cf6b6c945
Gabriela Alcântara <166942155+gabialcantar@users.noreply.github.com>
2025-01-06 22:20:43 -0300

    adição js
    Esse projeto tem como objetivo o projeto pessoal de 30 dias estudando js do básico até o avançado

---
+++ primeiro.js
@@ -0,0 +1,29 @@
+let nome = "Gabriela";
+const idade = 22;
+const fraseQueGosto = "Aceite recomeços por mais difíceis que sejam";
+
+// Exibindo no console
+console.log(nome);
+console.log(idade); 
+console.log("Eu " + nome + " gosto da frase: " + fraseQueGosto);
+
+
+// etapa 2
+let a = 10;
+let b = 15;
+let c = 50;
+
+let resultado = a + b + c;
+console.log(resultado)
+
+//etapa 3
+const minhaIdade = 22;
+const outraIdade = 25;
+
+if (minhaIdade > outraIdade) {
+    console.log("Minha idade é maior do que a outra.");
+} else if (minhaIdade < outraIdade) {
+    console.log("Minha idade é menor do que a outra.");
+} else {
+    console.log("Minha idade é igual à outra.");
+}
